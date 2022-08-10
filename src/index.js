bb8dda72097c60b9844ce1444a360e358932054a
Hugo Salles <salles.hugo05@gmail.com>
2022-08-10 15:33:38 -0400

    creating test processes for debugging

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export const sum = (a, b) => {
+
+    if(!a || !b) {
+        return "The input is incorrect!";
+    } if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'Only numbers are allowed'
+    }
+
+
+    return a + b;
+};
+
---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+import { sum } from '../src/index.js';
+
+it('should add 2 numbers and string and return the sum', () => {
+    // Arrange
+    const a = 4,
+     b = 5, expected = 9;
+
+    //Act
+    const actual = sum(a, b)
+
+    // Assert
+    expect(actual).toBe(expected)
+
+})
+it('should add a number and null and return an error', () => {
+    // Arrange
+    const a = null,
+     b = 5, expected = 'The input is incorrect!';
+
+    //Act
+    const actual = sum(a, b)
+
+    // Assert
+    expect(actual).toBe(expected)
+
+})
+
+it('should add a number and a string and return an error', () => {
+    // Arrange
+    const a = 'str',
+     b = 5, expected = 'are allowed';
+
+    //Act
+    const actual = sum(a, b)
+
+    // Assert
+    expect(actual).toContain(expected)
+
+})
+
